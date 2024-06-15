22fced0f4dbe72ea178402595666b3fdee22fe1e
Huy Tran <tanhuy998@gmail.com>
2024-06-16 03:44:02 +0700

    staging

--- web/src/pages/singleCampaignPage.js
+++ web/src/pages/singleCampaignPage.js
@@ -75,24 +75,27 @@ export default function SingleCampaignPage({ usecase }) {
                                 <div class="card-body">
                                     <h1 class="card-title">{campaignData?.title || 'Unknown'}</h1>
                                     <br />
-                                    <table>
-                                        <tr>
-                                            <td>
-                                                <h4 class="card-text">Ngày Bắt Đầu: </h4>
-                                            </td>
-                                            <td>
-                                                <h4 class="card-text">{campaignData?.issueTime || ''}</h4>
-                                            </td>
-                                            <td>
-                                                <h4 class="card-text">Ngày Kết Thúc: </h4>
-                                            </td>
-                                            <td>
-                                                <h4 class="card-text">{campaignData?.expire || ''}</h4>
-                                            </td>
-                                        </tr>
-                                    </table>
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col">
+                                                <h4 class="card-text">Ngày Bắt Đầu: {campaignData?.issueTime || ''}</h4>
+                                            </div>
+                                            <div class="col">
+                                                <h4 class="card-text">Ngày Kết Thúc: {campaignData?.expire || ''}</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br/>
+                                    <BasicTab tabs={{'Mô tả': `
 
-                                    <p class="card-text">This is some text within a card body...</p>
+Lorem ipsum dolor sit amet. Eos neque dolor id accusantium reprehenderit et impedit ipsam quo illo praesentium. Ut porro amet aut autem ducimus At voluptas repellat. Est accusamus sequi est fuga voluptate ut aliquid minima 33 dolores nisi est maxime aspernatur qui sunt voluptatum. Ad sequi iure nam vero quis et aliquam repellat et eaque soluta et galisum quaerat ut rerum esse.
+
+Qui nesciunt corporis in praesentium nemo ut vitae dolores et accusantium vitae. Ut nihil earum et iste quis id provident molestiae non voluptas veritatis ut ducimus similique et nulla pariatur et sint officia. In eius quibusdam qui facilis neque qui rerum consequatur. Qui reprehenderit possimus quo repellat quasi aut asperiores labore rem neque veritatis ut veniam distinctio est Quis dolor sit velit optio.
+
+A nisi aspernatur non natus aliquam aut mollitia rerum. Non magnam aperiam quo eligendi veritatis sit eaque perferendis et atque modi aut cumque odio ex dolorum provident! Ut vero impedit ad voluptatem optio ut Quis velit. Sit suscipit dolor sit voluptatem voluptatum eum inventore tenetur est quas quia qui eligendi minima.
+`}}/>
+                                    <br/>
+                                    
                                     <Link to="#" class="btn btn-primary">Chỉnh sửa</Link>
                                     <br />
 
@@ -100,7 +103,7 @@ export default function SingleCampaignPage({ usecase }) {
 
                                 <div class="card-body">
                                     <h3 class="card-title">Candidates Detail</h3>
-                                    <PillTab tabs={tabs}/>
+                                    <PillTab tabs={tabs} />
                                 </div>
 
                             </div>
